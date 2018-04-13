9928aa6463734a802c0bc9a8412544f93a9176c9
haneco <haneco@me.com>
2018-04-13 22:28:29 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = (homebridge) => {
-    const exportedTypes = {
-        Service: homebridge.hap.Service,
-        Characteristic: homebridge.hap.Characteristic,
-        Requester: require('./lib/Requester.js')
-    };
-    const DaikinAirconAccessory = require('./lib/DaikinAirconAccessory.js')(exportedTypes);
-    homebridge.registerAccessory('homebridge-daikin-aircon', 'DaikinAircon', DaikinAirconAccessory);
-}
+  const requester = require('./lib/Requester.js');
+  const exportedTypes = {
+    Service: homebridge.hap.Service,
+    Characteristic: homebridge.hap.Characteristic,
+    Requester: requester,
+  };
+  const DaikinAirconAccessory = require('./lib/DaikinAirconAccessory.js')(exportedTypes);
+  homebridge.registerAccessory('homebridge-daikin-aircon', 'DaikinAircon', DaikinAirconAccessory);
+};
--- test/DaikinAirconAccessory.test.js
+++ test/DaikinAirconAccessory.test.js
@@ -1,99 +1,97 @@
-const homebridge = new (require('homebridge/lib/api').API);
+const homebridge = new (require('homebridge/lib/api').API)();
+
 const Service = homebridge.hap.Service;
 const Characteristic = homebridge.hap.Characteristic;
-const Requester = require('./mock-Requester.js')
+const Requester = require('./mock-Requester.js');
 
 const exportedTypes = {
-    Service: Service,
-    Characteristic: Characteristic,
-    Requester: Requester
+  Service,
+  Characteristic,
+  Requester,
 };
 const DaikinAirconAccessory = require('../lib/DaikinAirconAccessory')(exportedTypes);
 
 describe('DaikinAirconAccessory', () => {
-    describe('parseResponse()', () => {
-        it('should be parsed', () => {
-            const accessory = new DaikinAirconAccessory();
-            let result;
-            
-            result = accessory.parseResponse('a=1,b=2');
-            expect(result).toEqual({
-                a: '1',
-                b: '2'
-            });
-        })
+  describe('parseResponse()', () => {
+    it('should be parsed', () => {
+      const result = DaikinAirconAccessory.parseResponse('a=1,b=2');
+      expect(result).toEqual({
+        a: '1',
+        b: '2',
+      });
     });
+  });
 
-    describe('getActive()', () => {
-        it('should be success', () => {
-            const accessory = new DaikinAirconAccessory();
-            accessory.getActive((error, value) => {
-                expect(error).toBeNull();
-                expect(value).toBe(Characteristic.Active.INACTIVE);
-            });
+  describe('getActive()', () => {
+    it('should be success', () => {
+      const accessory = new DaikinAirconAccessory();
+      accessory.getActive((error, value) => {
+        expect(error).toBeNull();
+        expect(value).toBe(Characteristic.Active.INACTIVE);
+      });
 
-            Requester.currentPower = '1';
-            accessory.getActive((error, value) => {
-                expect(error).toBeNull();
-                expect(value).toBe(Characteristic.Active.ACTIVE);
-            });
-        });
+      Requester.currentPower = '1';
+      accessory.getActive((error, value) => {
+        expect(error).toBeNull();
+        expect(value).toBe(Characteristic.Active.ACTIVE);
+      });
     });
+  });
 
-    describe('setActive()', () => {
-        it('should be active with valid mode', () => {
-            const config = {
-                coolingHeatingThreshold: 26
-            };
-            const accessory = new DaikinAirconAccessory(console.log, config);
-            
-            Requester.currentPower = '0';
+  describe('setActive()', () => {
+    it('should be active with valid mode', () => {
+      const config = {
+        coolingHeatingThreshold: 26,
+      };
+      const accessory = new DaikinAirconAccessory(console.log, config);
 
-            // 閾値より高い状態で運転開始
-            Requester.currentTemp = '28';
-            accessory.setActive(Characteristic.Active.ACTIVE, () => {
-                accessory.getActive((error, value) => {
-                    expect(error).toBeNull();
-                    expect(value).toBe(Characteristic.Active.ACTIVE);
-                });
+      Requester.currentPower = '0';
 
-                // 室温が閾値より高いので冷房
-                accessory.getHeaterCoolerState((error, value) => {
-                    expect(error).toBeNull();
-                    expect(value).toBe(Characteristic.CurrentHeaterCoolerState.COOLING);
-                });
-            });
-
-            // 閾値より高い状態で運転開始
-            Requester.currentTemp = '20';
-            accessory.setActive(Characteristic.Active.ACTIVE, () => {
-                accessory.getActive((error, value) => {
-                    expect(error).toBeNull();
-                    expect(value).toBe(Characteristic.Active.ACTIVE);
-                });
-
-                // 室温が閾値より低いので暖房
-                accessory.getHeaterCoolerState((error, value) => {
-                    expect(error).toBeNull();
-                    expect(value).toBe(Characteristic.CurrentHeaterCoolerState.HEATING);
-                });
-            });
+      // 閾値より高い状態で運転開始
+      Requester.currentTemp = '28';
+      accessory.setActive(Characteristic.Active.ACTIVE, () => {
+        accessory.getActive((error, value) => {
+          expect(error).toBeNull();
+          expect(value).toBe(Characteristic.Active.ACTIVE);
         });
 
-        it('should be inactive', () => {
-            const config = {
-                coolingHeatingThreshold: 26
-            };
-            const accessory = new DaikinAirconAccessory(console.log, config);
-            Requester.currentPower = '1';
-
-            // 運転停止
-            accessory.setActive(Characteristic.Active.INACTIVE, () => {
-                accessory.getActive((error, value) => {
-                    expect(error).toBeNull();
-                    expect(value).toBe(Characteristic.Active.INACTIVE);
-                });
-            });
+        // 室温が閾値より高いので冷房
+        accessory.getHeaterCoolerState((error, value) => {
+          expect(error).toBeNull();
+          expect(value).toBe(Characteristic.CurrentHeaterCoolerState.COOLING);
         });
+      });
+
+      // 閾値より高い状態で運転開始
+      Requester.currentTemp = '20';
+      accessory.setActive(Characteristic.Active.ACTIVE, () => {
+        accessory.getActive((error, value) => {
+          expect(error).toBeNull();
+          expect(value).toBe(Characteristic.Active.ACTIVE);
+        });
+
+        // 室温が閾値より低いので暖房
+        accessory.getHeaterCoolerState((error, value) => {
+          expect(error).toBeNull();
+          expect(value).toBe(Characteristic.CurrentHeaterCoolerState.HEATING);
+        });
+      });
     });
-})
+
+    it('should be inactive', () => {
+      const config = {
+        coolingHeatingThreshold: 26,
+      };
+      const accessory = new DaikinAirconAccessory(console.log, config);
+      Requester.currentPower = '1';
+
+      // 運転停止
+      accessory.setActive(Characteristic.Active.INACTIVE, () => {
+        accessory.getActive((error, value) => {
+          expect(error).toBeNull();
+          expect(value).toBe(Characteristic.Active.INACTIVE);
+        });
+      });
+    });
+  });
+});
